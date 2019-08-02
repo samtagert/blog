2a69d85498164c46550abe840b6363599cbaf9d3
Sam Tagert <samtagert@gmail.com>
2019-08-01 23:44:50 -0700

    fix for api overfetching for user ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,32 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // don't care about waiting until waiting until each user is fetched inside this function (end of the function)
+    userIds.forEach(id => dispatch(fetchUser(id)));
+    // optional _.chain syntax
+    // _.chain(getState().posts)
+    //     .map('userId')
+    //     .uniq()
+    //     .forEach(id => dispatch(fetchUser(id)))
+    //     .value();
+};
+
+// memoized approach, only able to fetch a user one time (aka profile screen not possible)
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
